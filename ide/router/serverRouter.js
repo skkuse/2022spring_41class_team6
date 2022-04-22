26b5e50112cc4a771e161211e22b0660412559ed
hhy4761 <ghdus1372@gmail.com>
2022-04-22 20:23:34 +0900

    router 수정

--- ide/router/serverRouter.js
+++ ide/router/serverRouter.js
@@ -4,16 +4,11 @@ const router = express.Router();
 const serverApis = require("../apis/server_apis");
 
 router.post('/register/:email/:password',(req,res,next)=>{
-    await serverApis.userRegister(req)
+    serverApis.userRegister(req)
 })
 
 router.get('/userInfo/:id',(req,res,next)=>{
-    let res = await DBManager.User.findOne({
-        where:{
-            id:req.params.id
-        }
-    });
-    return res;
+    serverApis.getUserInfo(req);
 })
 
 router.get('/emailOverlap',(req,res,next)=>{
@@ -21,5 +16,7 @@ router.get('/emailOverlap',(req,res,next)=>{
 })
 
 router.get('/problem/:id',(req,res,next)=>{
-    await serverApis.getProblem(req);
+    serverApis.getProblem(req);
 })
+
+module.exports = router;
