31f39b1443b08758bebbef8ba8f123b32189d030
--unset-all <--unset-all>
2022-05-13 14:20:32 +0900

    Revert "front2"
    This reverts commit 3f9682d1e6234097a798dab6ef14cbd3a479d3ee.

--- frontend/components/ProblemCard.js
+++ frontend/components/ProblemCard.js
@@ -4,14 +4,11 @@ export default function ProblemCard({pid,chapter,title})
 {
 
 
-    return (<Card style={{ width: '26rem' }}>
+    return (<Card style={{height :"fit-content", marginTop:"2rem"}} >
         <Card.Body>
             <Card.Title>챕터 {chapter} {pid}번 문제</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">{title}</Card.Subtitle>
             <Card.Text>
-                <Card.Text>문제설명</Card.Text>
-                <Card.Text>문제설명</Card.Text>
-                <Card.Text>문제설명</Card.Text>
                 <Card.Text>문제설명</Card.Text>
                 <Card.Text>문제설명</Card.Text>
                 <Card.Text>문제설명</Card.Text>
