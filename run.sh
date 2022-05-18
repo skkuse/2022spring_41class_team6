f0df4de084f408dcd15b46b62c29c358a8c0d257
JeHyunKim <kjh@TFG277>
2022-05-18 22:50:53 +0900

    modify run.sh

--- run.sh
+++ run.sh
@@ -12,6 +12,6 @@ sudo service docker start
 sudo docker ps
 sudo chmod 666 /var/run/docker.sock
 
-sudo docker run -d --net=host --name web --restart=always -v /var/run/docker.sock:/var/run/docker.sock \
+docker run -d --net=host --name web --restart=always -v /var/run/docker.sock:/var/run/docker.sock \
 -v $(which docker):/bin/docker -v $(pwd)/$seperator:$(pwd)/$seperator -w $(pwd)/$seperator \
 node:18 bash -c "npm install && npm run dev"
