443c78c0ffe6323e0880e00a1c669d7e9c82fcc1
JeHyun <kijen98@gmail.com>
2022-05-21 00:14:31 +0900

    run.sh에 mysql init 및 연결 대기 기능 추가. runcode.js 파일 생성하지 않아도 되도록 변경.

--- combined/pages/api/runcode.js
+++ combined/pages/api/runcode.js
@@ -1,46 +1,36 @@
 var execSync = require('child_process').execSync;
-var fs = require('fs');
 
 export default (req, res) => {
   if (req.method === "POST") {
     let code = req.body.code;
     let input = req.body.input;
     let id = 0;
-    const dir = `code/${id}/`;
-    if (!fs.existsSync(dir)) {
-    	fs.mkdirSync(dir, {
-    		recursive: true,
-        mode: 511
-    	});
-    }
-    fs.writeFile(dir + "input.txt", input, (err) => {
-      if (err) throw err;
-    })
-    fs.writeFile(dir + "main.py", code, (err) => {
-      var output = "";
-      try {
-        let options = {timeout : 10000,  };
-        output = execSync(`docker run --rm=True -v $(pwd)/${dir}:/app/ -w /app/ --name executor_${id} python:3.10 bash -c "python main.py < input.txt"`, options).toString();
-      } catch (err) {
-        switch(err.code) {
-          case "ETIMEDOUT":
-            output = "Execution timeout"
-            execSync(`docker stop executor_${id}`);
-            break;
-          case "ENOBUFS":
-            output = "Output Size is too large"
-            break;
-          default:
-            var errs = err.stderr.toString().split("\n");
-            if (errs[0].includes('WARNING: Error loading config file: ')){
-              output = errs.slice(1,-1).join("\n");
-            }
-            else {
-              output = errs.join("\n");
-            }
-        }
+
+    var output = "";
+    try {
+      let options = {timeout : 10000};
+      output = execSync(`docker run --rm=True --name executor_${id} python:3.10 \
+        bash -c "echo '${code}' > main.py && echo '${input}' > input.txt && \
+        python main.py < input.txt"`, options);
+    } catch (err) {
+      switch(err.code) {
+        case "ETIMEDOUT":
+          output = "Execution timeout"
+          execSync(`docker stop executor_${id}`);
+          break;
+        case "ENOBUFS":
+          output = "Output Size is too large"
+          break;
+        default:
+          var errs = err.stderr.toString().split("\n");
+          if (errs[0].includes('WARNING: Error loading config file: ')){
+            output = errs.slice(1,-1).join("\n");
+          }
+          else {
+            output = errs.join("\n");
+          }
       }
-      res.send(output);
-    });
+    }
+    res.send(output);
   }
 }
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-seperator='frontend'
+seperator='combined'
 
 docker -v
 
@@ -12,13 +12,19 @@ sudo service docker start
 sudo docker ps
 sudo chmod 666 /var/run/docker.sock
 
-docker pull node:18
+docker run -d --net=host --name mysql --restart=always -v $(pwd)/$seperator/db:/var/lib/mysql \
+-e MYSQL_ROOT_PASSWORD=qwer1234 mysql:8
+
 docker pull python:3.10
-docker pull mysql:8
+
+until nc -z -v -w30 localhost 3306
+do
+  echo "Waiting for database connection..."
+  sleep 5
+done
+
+docker exec mysql bash -c "mysqladmin -uroot -pqwer1234 create team6"
 
 docker run -d --net=host --name web --restart=always -v /var/run/docker.sock:/var/run/docker.sock \
 -v $(which docker):/bin/docker -v $(pwd)/$seperator:$(pwd)/$seperator -w $(pwd)/$seperator \
-node:18 bash -c "npm install && npm run dev"
-
-docker run -d --net=host --name mysql --restart=always -v $(pwd)/$seperator/db:/var/lib/mysql \
--e MYSQL_ROOT_PASSWORD=qwer1234 mysql:8
+node:18 bash -c "npm install && node server.js"
